77a76e299722379ff909787e1302d8a1a1953d81
Cantoral <lcantoral@dhcp80ff31e2.dynamic.uiowa.edu>
2011-11-14 15:56:20 -0600

    The "Featured Post" is just a sticky post, which the theme recognizes.

--- wp-content/themes/hawksnest/loop-index.php
+++ wp-content/themes/hawksnest/loop-index.php
@@ -30,7 +30,23 @@ query_posts(array('category_name' => 'birdfeeder', 'posts_per_page' => 10 ));
 
 // The Loop
 while ( have_posts() ) : the_post();
+if (is_sticky()) :
 ?>
+	<div id="featured"><div id="featured_post">
+<h2 class="entry-title">
+
+	<a href="<?php the_permalink(); ?>">
+	<?php the_title();?>
+	</a>
+</h2>
+	<div class="entry-content">
+				<?php the_excerpt();?>
+				
+	</div><!-- .entry-content -->
+	</div><!-- featured_post -->
+	</div><!-- featured -->
+<?php	else: ?>
+
 <h2 class="entry-title">
 
 	<a href="<?php the_permalink(); ?>">
@@ -42,6 +58,7 @@ while ( have_posts() ) : the_post();
 				
 	</div><!-- .entry-content -->
 <?php
+endif;
 endwhile;
 
 // Reset Query
