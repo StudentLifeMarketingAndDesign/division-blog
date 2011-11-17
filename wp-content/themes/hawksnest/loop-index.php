5636e7ed52ca3ba673562c75e27e76a06ec1f66e
Cantoral <lcantoral@dhcp80ff31e2.dynamic.uiowa.edu>
2011-11-17 14:41:44 -0600

    fixed how featured/sticky posts are called, implemented pagination

--- wp-content/themes/hawksnest/loop-index.php
+++ wp-content/themes/hawksnest/loop-index.php
@@ -1,36 +1,10 @@
 <?php
 
 // The Query
-query_posts(array('category_name' => 'featured', 'posts_per_page' => 1 ));
+query_posts(array('post__in' => get_option('sticky_posts'),'category_name' => 'birdfeeder', 'posts_per_page' => -1 ));
 
 // The Loop
 while ( have_posts() ) : the_post();
-?>
-<div id="featured"><div id="featured_post">
-<h2 class="entry-title">
-
-	<a href="<?php the_permalink(); ?>">
-	<?php the_title();?>
-	</a>
-</h2>
-	<div class="entry-content">
-				<?php the_excerpt();?>
-				
-	</div><!-- .entry-content -->
-	</div><!-- featured_post -->
-	</div><!-- featured -->
-<?php
-endwhile;
-
-// Reset Query
-wp_reset_query();
-
-// The Query
-query_posts(array('category_name' => 'birdfeeder', 'posts_per_page' => 10 ));
-
-// The Loop
-while ( have_posts() ) : the_post();
-if (is_sticky()) :
 ?>
 	<div id="featured"><div id="featured_post">
 <h2 class="entry-title">
@@ -40,12 +14,18 @@ if (is_sticky()) :
 	</a>
 </h2>
 	<div class="entry-content">
-				<?php echo apply_filters('the_content',get_the_content("Read More ...")) ?>
+				<?php the_content("Read More ..."); ?>
 				
 	</div><!-- .entry-content -->
 	</div><!-- featured_post -->
 	</div><!-- featured -->
-<?php	else: ?>
+<?php
+endwhile;
+wp_reset_query();
+$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+query_posts(array('post__not_in' => get_option('sticky_posts'),'category_name' => 'birdfeeder', 'posts_per_page' => 9,'paged' => $paged ));
+while ( have_posts() ) : the_post();
+?>
 <div class="post">
 <h2 class="entry-title">
 
@@ -54,13 +34,19 @@ if (is_sticky()) :
 	</a>
 </h2>
 	<div class="entry-content">
-				<?php echo apply_filters('the_content',get_the_content("Read More ...")) ?>
+				<?php the_content("Read More ..."); ?>
 				
 	</div><!-- .entry-content -->
 	</div><!-- .post -->
 <?php
-endif;
 endwhile;
+
+?>
+
+<div class="navigation"><p><?php posts_nav_link(); ?></p></div>
+
+<?php
+
 
 // Reset Query
 wp_reset_query();
